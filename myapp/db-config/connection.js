fc47376ea65034d71e7de7f6c2ba9a096b15d021
ashwinikedari <ganeshkedari@live.com>
2019-07-22 11:29:40 +0530

    Readme update

--- myapp/db-config/connection.js
+++ myapp/db-config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    //"mongodb+srv://m220student:m220password@mflix-vizvg.mongodb.net/imagica?retryWrites=true&w=majority"
+    //"mongodb+srv://<USERNAME>:<PASSOWORD>@<HOSTNAME>/imagica?retryWrites=true&w=majority"
     "mongodb://127.0.0.1:27017/imagica"
   )
   .then(() => {
