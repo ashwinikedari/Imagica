9f9181aa9ee94dc80694e03a261ce904fc6d9529
Ashwini <ashwinikedari@live.com>
2020-11-06 02:44:22 +0530

    Updated mongoDB Connection details

--- myapp/db-config/connection.js
+++ myapp/db-config/connection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    //"mongodb+srv://<USERNAME>:<PASSOWORD>@<HOSTNAME>/imagica?retryWrites=true&w=majority"
-    "mongodb://imagica_mongo:27017/imagica"
+    "mongodb://Ashwini_Kedari:Ashwini123@imagica_mongo:27017/imagica?retryWrites=true&w=majority"
+    // "mongodb://imagica_mongo:27017/imagica"
   )
   .then(() => {
     console.log("Connected to database!");
