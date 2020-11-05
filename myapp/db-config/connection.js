376a1af6cc0c00a9063a579a09776efee921794e
Ashwini <ashwinikedari@live.com>
2020-11-06 02:34:04 +0530

    MongoDB Connection Update

--- myapp/db-config/connection.js
+++ myapp/db-config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect(
     //"mongodb+srv://<USERNAME>:<PASSOWORD>@<HOSTNAME>/imagica?retryWrites=true&w=majority"
-    "mongodb://127.0.0.1:27017/imagica"
+    "mongodb://imagica_mongo:27017/imagica"
   )
   .then(() => {
     console.log("Connected to database!");
